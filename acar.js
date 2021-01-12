b62a4a96cf78e12dbbc36ab1649ba5323c5990fa
acar <77089894+acarfx@users.noreply.github.com>
2021-01-12 17:03:22 +0300

    Handler ve guildMemberAdd taşındı

--- acar.js
+++ acar.js
@@ -49,6 +49,38 @@ fs.readdir("./acar/Komutlar/", (err, dirs) => {
       );
     });
   });
+client.on("message", (message) => {
+    if([".link", "!link"].includes(message.content.toLowerCase())){ 
+    return message.channel.send(message.client.veri.sunucuDavetLinki); 
+    }
+    if([".tag", "!tag"].includes(message.content.toLowerCase())){ 
+      return message.channel.send(message.client.veri.Tag); 
+    }
+    if (message.author.bot || !message.content.startsWith(message.client.sistem.a_Prefix)) return;
+    let Args = message.content
+      .substring(message.client.sistem.a_Prefix.length)
+      .split(" ");
+    let Komut = Args[0];
+    let Acar = message.client;
+    Args = Args.splice(1);
+    let Baslatici;
+    let guild = message.guild || null;
+    if (guild != null)
+      await guild.fetch().then(result => guild = result)
+    if (Acar.komutlar.has(Komut)) {
+      Baslatici = Acar.komutlar.get(Komut);
+      if (Baslatici.TekSunucu && message.channel.type == "dm")
+        return;
+      Baslatici.onRequest(Acar, message, Args, guild);
+    } else if (Acar.komut.has(Komut)) {
+      Baslatici = Acar.komut.get(Komut);
+      if (Baslatici.TekSunucu && message.channel.type == "dm")
+        return;
+      Baslatici.onRequest(Acar, message, Args, guild);
+    }
+    bekleme[message.member.id] = Date.now();
+  }
+});
   client.gecmisTarihHesaplama = (date) => {
     const startedAt = Date.parse(date);
     var msecs = Math.abs(new Date() - startedAt);
